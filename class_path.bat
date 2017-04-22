e6f787396666c8153c9a06d3cc859bb13dd2e6dd
sudswain <sudswain@gmail.com>
2017-04-23 02:45:35 +0530

    index.html is added

---
+++ class_path.bat
@@ -0,0 +1,4 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_79\
+set MAVEN_HOME=D:\Sudhansu\Software\apache-maven-3.5.0-bin\apache-maven-3.5.0
+set path=D:\Sudhansu\Software\apache-maven-3.5.0-bin\apache-maven-3.5.0\bin;C:\Program Files\Java\jdk1.7.0_79\bin;C:\Users\sudhansu.swain\.jenkins\plugins\git\WEB-INF\lib;
+
